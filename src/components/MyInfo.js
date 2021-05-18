3120dd6a6e54a4f6059dd4e2b54c0149031b43d5
andrewcampbell <awr.campbell@gmail.com>
2021-05-18 15:49:04 -0400

    first React refresher update

---
+++ src/components/MyInfo.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function MyInfo() {
+  return (
+    <div>
+      <h1>Andrew Campbell</h1>
+      <p>Andrew is a HUMANOID that Steph loves</p>
+      <ul>
+        <li>Scotland</li>
+        <li>Vietnam</li>
+        <li>Belgium</li>
+      </ul>
+    </div>
+  )
+}
+
+export default MyInfo
